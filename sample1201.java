dff94112e4c143c95d12d5ddd90719f2db7627a2
akoromogi <joug.1618@gmail.com>
2015-01-20 21:01:15 +0900

    Update sample1201.java

--- sample1201.java
+++ sample1201.java
@@ -4,18 +4,12 @@ class houteisiki1d {
 	double b;
 
 	houteisiki1d(double a, double b){
-		this.a = a;
-		this.b = b;
 	}
 
 	String print(){
-		String formula = "1d: " + a + "x" + (b >= 0 ? "+" : "") + b + "=" + 0;
-		return formula;
 	}
 
 	double solve(){
-		double x = -b / a;
-		return x;
 	}
 }
 // x=-b / a
@@ -24,26 +18,17 @@ class houteisiki2d extends houteisiki1d {
 	int c;
 
 	houteisiki2d(int a, int b, int c){
-		super(a, b);
-		this.c = c;
 	}
 
 	String print(){
-		String formula = "2d: " + a + "x^2" + (b >= 0 ? "+" : "") + b + "x" + (c >= 0 ? "+" : "") + c + "=" + 0;
-		return formula;
 	}
 
 	double solve(){
-		double x = (-b + Math.sqrt(Math.pow(b, 2) - 4.0 * a * c)) / (2.0 * a);
-		return x;
 	}
 	
 	double solve2(){
-		double x = (-b - Math.sqrt(Math.pow(b, 2) - 4.0 * a * c)) / (2.0 * a);
-		return x;
 	}
 }
-// x^2/ab+x/a=-c/ab
 
 public class sample1201 {
 
@@ -51,15 +36,6 @@ public class sample1201 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		houteisiki1d test1d = new houteisiki1d(2, 3);
-		houteisiki2d test2d = new houteisiki2d(1, 8, 0);
-
-		System.out.println(test1d.print());
-		System.out.println(test1d.solve());
-		System.out.println(test2d.print());
-		System.out.println(test2d.solve());
-		System.out.println(test2d.solve2());
 	}
 
 }
